e5e1ac96bf552c8c2d8c1af4b53b8d8233f8bc2d
Anuar <anuar.jimenez@itexico.com>
2020-08-01 11:21:37 -0500

    Add services ciew

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../features/home/Home";
+import Services from "../features/services/Services";
 
 const AppRoutes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/servicios' render={() => {}} />
+      <Route path='/servicios' component={Services} />
       <Route path='/galeria' render={() => {}} />
       <Route path='/contacto' render={() => {}} />
     </Switch>
