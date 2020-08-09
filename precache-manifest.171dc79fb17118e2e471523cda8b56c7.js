8358544ec4320ca22f8a9736b343fa0a04955098
Anuar <anuar.jimenez@itexico.com>
2020-08-09 11:19:43 -0500

    Updates

---
+++ precache-manifest.171dc79fb17118e2e471523cda8b56c7.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76440f4d87c4ad38c33c8989df8b5b7a",
+    "url": "/visual-video/index.html"
+  },
+  {
+    "revision": "e6050e50dd3ad9c0e3be",
+    "url": "/visual-video/static/css/main.ea885651.chunk.css"
+  },
+  {
+    "revision": "aa67954348f6bcad26d6",
+    "url": "/visual-video/static/js/2.c2985853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/visual-video/static/js/2.c2985853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6050e50dd3ad9c0e3be",
+    "url": "/visual-video/static/js/main.5a68f74f.chunk.js"
+  },
+  {
+    "revision": "772fba1b16eaf511e99b",
+    "url": "/visual-video/static/js/runtime-main.23cd22e1.js"
+  },
+  {
+    "revision": "713bfe5909385bf2497396b03e25aba6",
+    "url": "/visual-video/static/media/candy-table-hq.713bfe59.jpg"
+  },
+  {
+    "revision": "3a4fae5772a12e5699bfbb1e740caa6c",
+    "url": "/visual-video/static/media/candy-table-lq.3a4fae57.jpg"
+  },
+  {
+    "revision": "0c2d890c834e1934718140a37d252f68",
+    "url": "/visual-video/static/media/candy-table-mq.0c2d890c.jpg"
+  },
+  {
+    "revision": "8c20e2f09ff1dae82ea85ef58fae7a3c",
+    "url": "/visual-video/static/media/cheers-2-hq.8c20e2f0.jpg"
+  },
+  {
+    "revision": "6a9611017c43b4e692db1d1ce5a6ebe6",
+    "url": "/visual-video/static/media/cheers-2-lq.6a961101.jpg"
+  },
+  {
+    "revision": "ce883a879a2d17b4215d912cf86229ae",
+    "url": "/visual-video/static/media/cheers-2-mq.ce883a87.jpg"
+  },
+  {
+    "revision": "28260131e2911267c63b093aad6c6335",
+    "url": "/visual-video/static/media/dvd-cover-hq.28260131.jpg"
+  },
+  {
+    "revision": "f01ced303b23443ceb52164508d70b5a",
+    "url": "/visual-video/static/media/dvd-cover-lq.f01ced30.jpg"
+  },
+  {
+    "revision": "016827c09d2f3fa20bb77b8846ffbc56",
+    "url": "/visual-video/static/media/dvd-cover-mq.016827c0.jpg"
+  },
+  {
+    "revision": "710ccb5fd250191fe4748b7c77f192a2",
+    "url": "/visual-video/static/media/flower-field-edited-hq.710ccb5f.jpg"
+  },
+  {
+    "revision": "cd27c560d42298535b903e320f7b1b70",
+    "url": "/visual-video/static/media/flower-field-edited-lq.cd27c560.jpg"
+  },
+  {
+    "revision": "c59380cf6596f78dac3d8c0cc490562f",
+    "url": "/visual-video/static/media/flower-field-edited-mq.c59380cf.jpg"
+  },
+  {
+    "revision": "203c5df059a665876071d3be95d3d5db",
+    "url": "/visual-video/static/media/flower-field-edited.203c5df0.png"
+  },
+  {
+    "revision": "46f994a772bf294fb2497b65287e8bc2",
+    "url": "/visual-video/static/media/give-a-hand.46f994a7.png"
+  },
+  {
+    "revision": "f089fd5d7ddeab1b05cae89d13bb37cc",
+    "url": "/visual-video/static/media/greet-hq.f089fd5d.jpg"
+  },
+  {
+    "revision": "9c1f923ab55d1f057da7d1e5ba7ba890",
+    "url": "/visual-video/static/media/greet-lq.9c1f923a.jpg"
+  },
+  {
+    "revision": "230e7571d52fb542f9e073a4de901133",
+    "url": "/visual-video/static/media/greet-mq.230e7571.jpg"
+  },
+  {
+    "revision": "70e8a214b19793bddf89c626e9f77b78",
+    "url": "/visual-video/static/media/hero-gallery-hq.70e8a214.jpg"
+  },
+  {
+    "revision": "99c9f6a0376d28736ee7d585f76bd207",
+    "url": "/visual-video/static/media/hero-gallery-lq.99c9f6a0.jpg"
+  },
+  {
+    "revision": "82d90015f41bdde8c54dc89d2a6d40d1",
+    "url": "/visual-video/static/media/hero-gallery-mq.82d90015.jpg"
+  },
+  {
+    "revision": "0143ad28fa9440a4bbe5f2d25808ce46",
+    "url": "/visual-video/static/media/hero-hq.0143ad28.jpg"
+  },
+  {
+    "revision": "fb9d48acd9dd7cd91f86265e25490e00",
+    "url": "/visual-video/static/media/hero-lq.fb9d48ac.jpg"
+  },
+  {
+    "revision": "eeca8303438a9e91916409ee94df3532",
+    "url": "/visual-video/static/media/hero-mq.eeca8303.jpg"
+  },
+  {
+    "revision": "a98766aacadfbbb8697d208426882b20",
+    "url": "/visual-video/static/media/invitation-hq.a98766aa.jpg"
+  },
+  {
+    "revision": "ffd01400ad16e7d1e9ba1f245f81d7d2",
+    "url": "/visual-video/static/media/invitation-lq.ffd01400.jpg"
+  },
+  {
+    "revision": "e665a4dc19014c9b7a6b0762545dfc54",
+    "url": "/visual-video/static/media/invitation-mq.e665a4dc.jpg"
+  },
+  {
+    "revision": "f6d8979369baa3f0a4ef00fcdaef6112",
+    "url": "/visual-video/static/media/kiss-hq.f6d89793.jpg"
+  },
+  {
+    "revision": "95bbcd7dc0e1176a2ffaa31166c8726d",
+    "url": "/visual-video/static/media/kiss-lq.95bbcd7d.jpg"
+  },
+  {
+    "revision": "c0bcb2a76569df4a74a31270be97d7ec",
+    "url": "/visual-video/static/media/kiss-mq.c0bcb2a7.jpg"
+  },
+  {
+    "revision": "f85a6383a9ab5ff6aa51f4e6561171a7",
+    "url": "/visual-video/static/media/slice-of-cake-hq.f85a6383.jpg"
+  },
+  {
+    "revision": "d5eb17eebe54d263f6ef4b63f889847d",
+    "url": "/visual-video/static/media/slice-of-cake-lq.d5eb17ee.jpg"
+  },
+  {
+    "revision": "483a1d990172493595cee60020fef988",
+    "url": "/visual-video/static/media/slice-of-cake-mq.483a1d99.jpg"
+  },
+  {
+    "revision": "2b818ed92ac64e3dd8d00af245cb2d81",
+    "url": "/visual-video/static/media/transparent-white.2b818ed9.png"
+  },
+  {
+    "revision": "bbd7bd3ee587ab12f15866a91e7bdf60",
+    "url": "/visual-video/static/media/trincket-table-hq.bbd7bd3e.jpg"
+  },
+  {
+    "revision": "dd5f8e8c6c8bdad41d040df399b3f5f0",
+    "url": "/visual-video/static/media/trincket-table-lq.dd5f8e8c.jpg"
+  },
+  {
+    "revision": "38e5eeecae2f8a524714c1838215cbbd",
+    "url": "/visual-video/static/media/trincket-table-mq.38e5eeec.jpg"
+  },
+  {
+    "revision": "853fb7d75c9f5d9afb02d0532766543d",
+    "url": "/visual-video/static/media/us-against-hq.853fb7d7.jpg"
+  },
+  {
+    "revision": "e486f75ecf481b709bed45dc9d49b9d0",
+    "url": "/visual-video/static/media/us-against-lq.e486f75e.jpg"
+  },
+  {
+    "revision": "5af261fe90c7a8f388326a0813388366",
+    "url": "/visual-video/static/media/us-against-mq.5af261fe.jpg"
+  }
+]);
